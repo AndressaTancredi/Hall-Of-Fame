6c17d8dfbb330fdbf77cbfeef1ff5af941a988f9
Andressa Tancredi <andressa.tancredi@hotmail.com>
2023-05-17 16:56:30 -0300

    add text styles

---
+++ lib/common/text_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'app_colors.dart';
+
+class TextStyles {
+  final body = const TextStyle(
+    fontFamily: "TTTravels",
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    fontStyle: FontStyle.normal,
+    color: AppColors.primary,
+  );
+
+  final titleDarkBold = const TextStyle(
+    fontFamily: "TTTravels",
+    fontWeight: FontWeight.w700,
+    fontSize: 18,
+    fontStyle: FontStyle.normal,
+    color: AppColors.primary,
+  );
+}
