63ba687ff7bdffac29d891a8e616f4a1dc07e396
Andressa Tancredi <andressa.tancredi@hotmail.com>
2023-05-17 10:52:51 -0300

    set button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../common/app_colors.dart';
 import '../common/common_strings.dart';
+import '../widget/button.dart';
 import '../widget/dropdown_field_form.dart';
 import '../widget/text_field_form.dart';
 
@@ -40,30 +41,7 @@ class _HomePageState extends State<HomePage> {
               ),
               const SizedBox(height: 24.0),
               TextFieldForm(title: CommonStrings.voucher),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      elevation: 0,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0)),
-                      minimumSize: const Size(180, 45),
-                    ),
-                    onPressed: () => Navigator.of(context).pushNamed(''),
-                    child: Text(
-                      CommonStrings.start,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w700,
-                        color: AppColors.black60,
-                        fontStyle: FontStyle.normal,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+              const Button(),
             ],
           ),
         ),
