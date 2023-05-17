2f4a70cbf937b29e807926e04eb2ba4665ee15f4
Andressa Tancredi <andressa.tancredi@hotmail.com>
2023-05-17 10:49:01 -0300

    create common strings

---
+++ lib/common/common_strings.dart
@@ -0,0 +1,10 @@
+class CommonStrings {
+  static String get name => "Nome";
+  static String get yearsOld => "Idade";
+  static String get gender => "Gênero";
+  static String get voucher => "Voucher";
+  static String get start => "Começar";
+
+  static List<String> get genderList =>
+      ['Masculino', 'Feminino', 'Prefiro não informar'];
+}
