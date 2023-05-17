914dfbad3874000fc6f2b2fd87f2f8cdcca280fc
Andressa Tancredi <andressa.tancredi@hotmail.com>
2023-05-17 10:47:10 -0300

    refactor: adjust theme

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: 'Calçada da Fama',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const HomePage(),
