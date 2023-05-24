645454db0123ea3b83a0b7d5aaba3e31e2e37ffa
Andressa Tancredi <andressa.tancredi@hotmail.com>
2023-05-24 16:19:09 -0300

    create final image to share

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,44 +1,40 @@
-import 'dart:math';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:share/share.dart';
 
 import '../common/app_colors.dart';
 import '../common/common_strings.dart';
 import '../common/injection_container.dart';
 import '../common/text_styles.dart';
+import '../common/utils.dart';
 import '../widget/button.dart';
+import '../widget/widgetToImage.dart';
 
 class ResultPage extends StatefulWidget {
   final Uint8List photo;
   const ResultPage({Key? key, required this.photo}) : super(key: key);
-
-  static const List<String> imageAssets = [
-    'assets/images/image1.jpg',
-    'assets/images/image2.jpg',
-    'assets/images/image3.jpg',
-  ];
 
   @override
   State<ResultPage> createState() => _ResultPageState();
 }
 
 class _ResultPageState extends State<ResultPage> {
+  late GlobalKey key1;
+  late Uint8List bytes1;
   TextStyle get titleStyle => sl<TextStyles>().titleYellow;
 
   @override
   Widget build(BuildContext context) {
-    final random = Random();
-    final int randomIndex = random.nextInt(ResultPage.imageAssets.length);
-    final String backgroundImage = ResultPage.imageAssets[randomIndex];
     return SafeArea(
       child: Scaffold(
         backgroundColor: AppColors.primary,
         body: SingleChildScrollView(
           child: Padding(
             padding:
-                const EdgeInsets.symmetric(vertical: 150.0, horizontal: 40.0),
+                const EdgeInsets.symmetric(vertical: 50.0, horizontal: 40.0),
             child: Column(
               children: [
                 Center(
@@ -48,38 +44,48 @@ class _ResultPageState extends State<ResultPage> {
                     textAlign: TextAlign.center,
                   ),
                 ),
-                const SizedBox(height: 150),
+                const SizedBox(height: 50),
                 Container(
-                  padding: const EdgeInsets.all(10.0),
                   decoration: BoxDecoration(
-                    color: AppColors.black15,
                     border: Border.all(
-                        color: AppColors.yellow.withOpacity(0.5), width: 10),
+                        color: AppColors.yellow.withOpacity(0.5), width: 5),
                     borderRadius: const BorderRadius.all(Radius.circular(40)),
                   ),
-                  child: Stack(
-                    children: [
-                      Image.asset(
-                        backgroundImage,
-                        fit: BoxFit.fill,
-                        width: 1300,
-                        height: 900,
+                  child: WidgetToImage(builder: (key) {
+                    key1 = key;
+                    return ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(40)),
+                      child: Stack(
+                        children: [
+                          Image.asset(
+                            'assets/images/hallOfFame.jpg',
+                            fit: BoxFit.contain,
+                            height: 1300,
+                          ),
+                          Positioned.fill(
+                            bottom: -650,
+                            left: 120,
+                            child: Image.memory(
+                              height: 1,
+                              widget.photo,
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                        ],
                       ),
-                      Positioned.fill(
-                        child: Image.memory(
-                          widget.photo,
-                          fit: BoxFit.contain,
-                        ),
-                      ),
-                    ],
-                  ),
+                    );
+                  }),
                 ),
-                const SizedBox(height: 150.0),
-                Button(
-                  title: CommonStrings.share.toUpperCase(),
-                  onPressed: () async {
-                    await _shareMergedImages(backgroundImage, widget.photo);
-                  },
+                const SizedBox(height: 70.0),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 130.0),
+                  child: Button(
+                    title: CommonStrings.share.toUpperCase(),
+                    onPressed: () async {
+                      bytes1 = await Utils.capture(key1);
+                      await _shareMergedImages(bytes1);
+                    },
+                  ),
                 ),
               ],
             ),
@@ -89,12 +95,12 @@ class _ResultPageState extends State<ResultPage> {
     );
   }
 
-  Future<void> _shareMergedImages(
-    String backgroundImage,
-    Uint8List photo,
-  ) async {
-    const mergedImagePath = ""; // criar!
+  Future<void> _shareMergedImages(Uint8List bytes) async {
+    final tempDir = await getTemporaryDirectory();
+    final tempFilePath = '${tempDir.path}/merged_image.jpg';
+    final tempFile = File(tempFilePath);
+    await tempFile.writeAsBytes(bytes1);
 
-    Share.shareFiles([mergedImagePath]);
+    await Share.shareFiles([tempFilePath]);
   }
 }
